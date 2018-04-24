8d14170a186627d4ea0ac58808ad3e55470e3d39
Allyson Aberg <allya7897@gmail.com>
2018-04-24 12:59:26 -0400

    buttons and basic layout added

--- countdown.py
+++ countdown.py
@@ -1,47 +1,78 @@
+#TODO:
+#reset button functionality
+#other buttons (for actual stops)
+#keyboard (numbers)
+
 #!/usr/bin/python
 import sys
 #for mac running python3
-#import Tkinter as TK
-#from Tkinter import *
+# import tkinter as TK
+# from tkinter import *
 
 #for the pi running python3
 import Tkinter as TK
 from Tkinter import *
-import time
 
 #window stuff
 window = TK.Tk()
 window.title('countdown timer')
-window.geometry("100x100")
+window.geometry("500x200")
 
+mainFrame = Frame(window)
+mainFrame.pack(expand=True)
+
+global isRunning
+
+isRunning = False
 
 def countdown(i):
-	clock['text'] = i
-
-	if i > 0:
-		#run again after 1000 ms/1s
-		window.after(1000, countdown, i - 1)
-	else:
-		clock['text'] = 'DONE'
+	global isRunning
+	if isRunning:
+		clock['text'] = i
+		if i > 0:
+			#run again after 1000 ms/1s
+			window.after(1000, countdown, i - 1)
+		else:
+			print('done')
+			clock['text'] = '0'
+			done['text'] = 'START'
+			isRunning = False
 
 def run():
-	# countdown(timeLeft)
-	count = IntVar()
-	count = timeLeft.get()
-	countdown(int(count))
+	global isRunning
+
+	continue_count = False
+
+	if isRunning:
+		done['text'] = '...'
+		#stop timer
+		isRunning = False
+	else:
+		if done['text'] == '...':
+			count = clock['text']
+		else:
+			count = IntVar()
+			count = timeLeft.get()
+
+		done['text'] = 'STOP'
+		isRunning = True
+		countdown(int(count))
 
 
-# timeLeft= int(input("HOW LONG?"))
-timeLeft = Entry(window)
+timeLeft = Entry(mainFrame)
 timeLeft.pack()
 timeLeft.focus_set()
+timeLeft.config(width=3)
 
-clock = TK.Label(window, bg='red')
-clock.place(x = 55, y = 55)
+clock = TK.Label(mainFrame, font = (None,26), text='0')
+clock.pack()
 
-done = Button(window, text='done', command=run)
-done.pack(side='bottom')
+done = Button(mainFrame, text='START', command=run)
+done.pack()
 
+
+reset = Button(mainFrame, text='RESET')
+reset.pack()
 
 window.mainloop()
 
