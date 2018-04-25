1472ee1597a18afcb126759e7745bc166af9d657
Allyson Aberg <allya7897@gmail.com>
2018-04-24 20:01:57 -0400

    more fstop functionality

--- countdown.py
+++ countdown.py
@@ -1,34 +1,42 @@
 #TODO:
-#reset button functionality
 #other buttons (for actual stops)
 #keyboard (numbers)
+#does it have miliseconds?
 
 #!/usr/bin/python
 import sys
 #for mac running python3
-#import tkinter as TK
-#from tkinter import *
+import tkinter as TK
+from tkinter import *
 
 #for the pi running python3
-import Tkinter as TK
-from Tkinter import *
-
-#window stuff
-window = TK.Tk()
-window.title('countdown timer')
-window.geometry("500x200")
-
-mainFrame = Frame(window)
-mainFrame.pack(expand=True)
+# import Tkinter as TK
+# from Tkinter import *
 
 global isRunning
 global reset_it
 global testStopValue
-
 testStopValue = 0
 isRunning = False
 reset_it = False
 
+
+################### WINDOW SETUP ##########################   
+window = TK.Tk()
+window.title('countdown timer')
+window.geometry("500x300")
+
+mainFrame = Frame(window)
+mainFrame.pack()
+
+topFrame = Frame(window)
+topFrame.pack()
+
+bottomFrame = Frame(window)
+bottomFrame.pack()
+################### WINDOW SETUP ##########################   
+
+################### FUNCTIONAL METHODS ##########################   
 def countdown(i):
 	global isRunning
 	global reset_it
@@ -70,7 +78,7 @@ def run():
 				count = clock['text']
 			else:
 				count = IntVar()
-				count = timeLeft.get()
+				count = int(timeLeft.get()) + int(testStopValue)
 
 			done['text'] = 'STOP'
 			isRunning = True
@@ -81,48 +89,80 @@ def reset():
 	reset_it = True
 	run()
 
-def up():
+def up(val):
     global testStopValue
-    print('up')
     testStopValue +=1
-    testStop['text'] = testStopValue
+    updateTextButton()
 
-def down():
+def down(val):
     global testStopValue
-    print('down')
-    testStopValue -=1
-    testStop['text'] = testStopValue
+    if int(testStopValue) > 0:
+    	testStopValue -=1
+    	updateTextButton()
+################### FUNCTIONAL METHODS ##########################   
 
-    
+################### UPDATE METHODS ##########################   
 def updatetext(event):
     clock['text'] = int(timeLeft.get()) + int(testStopValue)
     window.update_idletasks()  
 
+def updateTextButton():
+    testStop['text'] = testStopValue
+    clock['text'] = int(timeLeft.get()) + int(testStopValue)
+    window.update_idletasks()
+################### UPDATE METHODS ##########################   
+
+
+################### FUNCTIONAL BUTTONS ##########################
 timeLeft = Entry(mainFrame)
-timeLeft.pack()
+timeLeft.grid(row=0,column=7, ipadx = 10)
 timeLeft.focus_set()
-timeLeft.config(width=3)
+timeLeft.config(width=4)
 timeLeft.bind(sequence='<KeyRelease>', func=updatetext)
 
-testStopValue = 0
-testStop = TK.Label(mainFrame, font=(None, 20), text = testStopValue)
-testStop.pack()
-
-testStopButton1 = Button(mainFrame, text='+', command=up)
-testStopButton1.bind(sequence='<KeyRelease>', func=updatetext)
-testStopButton1.pack()
-
-testStopButton2 = Button(mainFrame, text='-', command=down)
-testStopButton2.pack()
-
-clock = TK.Label(mainFrame, font = (None,45), text=timeLeft.get())
+clock = TK.Label(bottomFrame, font = (None,45), text=timeLeft.get())
 clock.pack()
 
-done = Button(mainFrame, text='START', command=run)
+done = Button(bottomFrame, text='START', command=run)
 done.pack()
 
-reset = Button(mainFrame, text='RESET', command=reset)
+reset = Button(bottomFrame, text='RESET', command=reset)
 reset.pack()
+################### FUNCTIONAL BUTTONS ##########################
+
+
+################### F/STOP BUTTONS ##########################
+testStopValue = 0
+testStop = TK.Label(topFrame, font=(None, 15), text = testStopValue)
+testStop.grid(row=1,column=3)
+
+testAddButton = Button(topFrame, text='+', command=up)
+testAddButton.grid(row=5,column=4)
+
+testSubButton = Button(topFrame, text='-', command=down)
+testSubButton.grid(row=5,column=2)
+
+testStopValue1 = 0
+testStop = TK.Label(topFrame, font=(None, 15), text = testStopValue1)
+testStop.grid(row=1,column=7)
+
+testAddButton1 = Button(topFrame, text='+', command=up)
+testAddButton1.grid(row=5,column=8)
+
+testSubButton1 = Button(topFrame, text='-', command=down)
+testSubButton1.grid(row=5,column=6)
+
+testStopValue2 = 0
+testStop = TK.Label(topFrame, font=(None, 15), text = testStopValue2)
+testStop.grid(row=1,column=11)
+
+testAddButton2 = Button(topFrame, text='+', command=up)
+testAddButton2.grid(row=5,column=12)
+
+testSubButton2 = Button(topFrame, text='-', command=down)
+testSubButton2.grid(row=5,column=10)
+################### F/STOP BUTTONS ##########################
+
 
 window.mainloop()
 
